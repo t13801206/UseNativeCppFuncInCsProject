3af6c358ed86bc349f995cc4d10d6a04434068c0
t13801206 <t13801206@yahoo.co.jp>
2022-03-09 04:52:55 +0900

    add new project that use C++ directly

---
+++ CppNativeForDotnetCore31/framework.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
+// Windows ヘッダー ファイル
+#include <windows.h>
+
+#define DllExport   __declspec( dllexport )
+
+extern "C"
+{
+    DllExport void CheckOpenCV();
+    DllExport void SendCommand(char*, int);
+}
--- CsMainUser/Program.cs
+++ CsMainUser/Program.cs
@@ -14,9 +14,15 @@ namespace CsMainUser
         [STAThread]
         static void Main()
         {
+            // OSが何ビットで動作しているか確認
+            Console.WriteLine($"OS : {(Environment.Is64BitOperatingSystem ? "64bit" : "32bit")}");
+            // プロセスが何ビットで動作しているか確認
+            Console.WriteLine($"プロセス : {(Environment.Is64BitProcess ? "64bit" : "32bit")}");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
